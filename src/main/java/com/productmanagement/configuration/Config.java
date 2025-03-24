66658784e8581a4b433a5e08f37fd17e7367df67
laura-barbulescu <lauraionela.barbulescu@gmail.com>
2025-03-24 19:17:25 +0200

    Initial commit

---
+++ src/main/java/com/productmanagement/configuration/Config.java
@@ -0,0 +1,9 @@
+package com.productmanagement.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.productmanagement.persistence.repository")
+public class Config {
+}
